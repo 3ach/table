940120ccb1503981b68258a40463a321e10b168d
Zach Zundel <me@zachzundel.com>
2026-01-16 08:47:05 -0800

    rect -> path for calibration square

--- src/components/CalibrationSquare.tsx
+++ src/components/CalibrationSquare.tsx
@@ -9,8 +9,13 @@ interface CalibrationSquareProps extends SVGProps {
 export default class CalibrationSquare extends SVGComponent<CalibrationSquareProps> {
     svg() {
 		const calibrationSquareSize = this.props.table.calibrationSquareSize;
+		const pathstr = `M 0 0 `
+					  + `L 0 ${calibrationSquareSize} ` 
+					  + `L ${calibrationSquareSize} ${calibrationSquareSize} ` 
+					  + `L ${calibrationSquareSize} 0 ` 
+					  + `z`;
 
-		return SVG().rect(calibrationSquareSize, calibrationSquareSize)
+		return SVG().path(pathstr)
 			.fill("none")
 			.attr('vector-effect', 'non-scaling-stroke');
     }
